7b04d2ebc8d0057df1e7a93464d55abe98505496
alexandrebk <alexandre.b2506@gmail.com>
2018-06-02 09:56:47 +0200

    final commit

--- src/components/flat.jsx
+++ src/components/flat.jsx
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 class Flat extends Component {
   render() {
     return (
-      <div class="card active" style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.2)), url('${this.props.imageUrl}')` }}>
-        <div class="card-category">{this.props.price}</div>
-        <div class="card-description">
+      <div className="flat card active" style={{ width: "400px", backgroundImage: `linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.7)), url('${this.props.imageUrl}')` }}>
+        <div className="card-category">{this.props.price}</div>
+        <div className="card-description">
           <h2 onClick={console.log("==> debug")}> {this.props.name} <a class="card-link" href="#">TEST</a></h2>
         </div>
       </div>
